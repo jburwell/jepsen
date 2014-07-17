6579556aa6ae56f05dd82c439d316d2109711875
John Burwell <jburwell@basho.com>
2014-07-17 12:59:38 -0400

    - Moves riak client creation to the create-riak-client function

--- riak2/src/jepsen/system/riak2.clj
+++ riak2/src/jepsen/system/riak2.clj
@@ -24,13 +24,16 @@
   )
 )
 
+(defn- create-riak-client
+    [host]
+    (let [riak_node (.. (RiakNode$Builder.) (withRemoteAddress host) build)]
+        (.. (RiakCluster$Builder. riak_node) build)))
+
 (defrecord CreateSetClient [client]
   client/Client
-  (setup! [this test node]
-    (info "Setting up a client for node " node)
-    (CreateSetClient. 
-           (let [riak_node (.. (RiakNode$Builder.) (withRemoteAddress node) build)]
-             (.. (RiakCluster$Builder. riak_node) build))))
+  (setup! [this test host]
+    (info "Setting up a client for node " host)
+    (CreateSetClient. (create-riak-client host)))
   
   (invoke! [this test op]
     (case (:f op)
