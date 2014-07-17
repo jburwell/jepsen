2b60396e3acf1bad557cb3afe39b804730347c18
John Burwell <jburwell@basho.com>
2014-07-17 12:15:12 -0400

    - Fix the instantiation scopes of the riak_node when creating a   RiakCluster

--- riak2/src/jepsen/system/riak2.clj
+++ riak2/src/jepsen/system/riak2.clj
@@ -24,13 +24,13 @@
   )
 )
 
-(defrecord SetClient [client]
+(defrecord CreateSetClient [client]
   client/Client
   (setup! [this test node]
     (info "Setting up a client for node " node)
-    (let [riak_node (..  (RiakNode$Builder.) (withRemoteAddress node))
-          client (.. (RiakCluster$Builder. riak_node) build)])
-    (assoc this :client client))
+    (CreateSetClient. 
+           (let [riak_node (.. (RiakNode$Builder.) (withRemoteAddress node) build)]
+             (.. (RiakCluster$Builder. riak_node) build))))
   
   (invoke! [this test op]
     (case (:f op)
@@ -41,4 +41,4 @@
     (.close client)))
 
 (defn create-set-client []
-  (SetClient. nil))
+  (CreateSetClient. nil))
