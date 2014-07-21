a62051808a66027a06de2b67c6cc8710b4cac7ce
John Burwell <jburwell@basho.com>
2014-07-21 16:47:45 -0400

    - Refactors client creation to use the updated 2.0 API

--- riak2/src/jepsen/system/riak2.clj
+++ riak2/src/jepsen/system/riak2.clj
@@ -2,8 +2,12 @@
    (:require [clojure.tools.logging     :refer [info]]
              [jepsen.client             :as client]
              [jepsen.db                 :as db])
-   (:import [com.basho.riak.client.core RiakCluster$Builder]
-            [com.basho.riak.client.core RiakNode$Builder]))
+   (:import [java.nio.charset Charset]
+            [java.util UUID]
+            [com.basho.riak.client.api RiakClient]
+            [com.basho.riak.client.core.query Namespace]))
+
+(def set-bucket-type "set-type")
 
 (def db
   (reify db/DB
@@ -24,17 +28,29 @@
   )
 )
 
-(defn- create-riak-client
-    [host]
-    (let [riak_node (.. (RiakNode$Builder.) (withRemoteAddress host) build)]
-        (.. (RiakCluster$Builder. riak_node) build)))
+(defn- symbol-to-str
+  [symbol]
+  (apply str (rest (str symbol))))
 
-(defrecord CreateSetClient [client]
+(defn- create-bucket
+  [type name]
+  (-> (Namespace. type name)))
+
+(defn- create-random-bucket
+  [type]
+  (create-bucket type (-> (UUID/randomUUID) .toString)))
+
+(defn- start-riak-client
+    [host]
+    (RiakClient/newClient (cons host)))
+
+(defrecord CreateSetClient [client bucket]
   client/Client
   (setup! [this test host]
     (info "Setting up a client for node " host)
-    (CreateSetClient. (create-riak-client host)))
-  
+    (let [riak-client (start-riak-client (symbol-to-str host))]
+      (CreateSetClient. riak-client bucket)))
+    
   (invoke! [this test op]
     (case (:f op)
       :add ()
@@ -44,4 +60,5 @@
     (.close client)))
 
 (defn create-set-client []
-  (CreateSetClient. nil))
+    (let [bucket (create-random-bucket set-bucket-type)]
+        (CreateSetClient. nil, bucket)))
